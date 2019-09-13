09d16139aea6034994439bc001ec73977927631e
Faisal Khan <faisalkhan1987@gmail.com>
2019-09-13 22:35:19 +0200

    Initial commit

---
+++ src/main/java/com/fsl/eshop/eshopservice/EshopServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fsl.eshop.eshopservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopServiceApplication.class, args);
+	}
+
+}
